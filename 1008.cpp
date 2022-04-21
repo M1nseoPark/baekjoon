24138cba08f069404f4bee54ad2fb925467c80a1
MinseoPark <kjms0820@gmail.com>
2022-04-21 21:31:23 +0900

    119 day

---
+++ 1008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	printf("%.9f", (double)a / (double)b);
+
+	return 0;
+}
---
+++ 1008.py
@@ -0,0 +1,3 @@
+a, b = map(int, input().split())
+output = "{:.9f}".format(a / b)
+print(output)
